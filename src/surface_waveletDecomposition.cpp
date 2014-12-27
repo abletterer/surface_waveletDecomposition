c230c74c0816103715edbfe30547920a767e6942
Arnaud Bletterer <arnaud.bletterer@gmail.com>
2014-12-27 12:16:27 +0100

    [NON-FONCTIONNEL] Reprojection des points du plan 2D dans l'espace 3D

--- src/surface_waveletDecomposition.cpp
+++ src/surface_waveletDecomposition.cpp
@@ -105,8 +105,7 @@ void Surface_WaveletDecomposition_Plugin::decompose()
     if(m_decomposition)
     {
         bool stop = false;
-        int width = m_decomposition->getWidth();
-        int height = m_decomposition->getHeight();
+        int width = m_decomposition->getWidth(), height = m_decomposition->getHeight();
         int img_width = width, img_height = height;
         int img_width2, img_height2;
         while(!stop)
@@ -180,10 +179,13 @@ void Surface_WaveletDecomposition_Plugin::decompose()
 
             img_width  = img_width2;
             img_height = img_height2;
-            m_decomposition->getDownDecomposition();
             if(img_width < 2 || img_height < 2)
             {
                 stop = true;
+            }
+            else
+            {
+                m_decomposition->getDownDecomposition();
             }
         }
     }
@@ -220,206 +222,166 @@ void Surface_WaveletDecomposition_Plugin::saveImages(const QString& name, const 
 
 MapHandlerGen* Surface_WaveletDecomposition_Plugin::drawCoarseImage(const QString& mapName)
 {
-//    if(m_decomposition)
-//    {
-//        MapHandlerGen* mhg_map = m_schnapps->addMap(mapName, 2);
-//        MapHandler<PFP2>* mh_map = static_cast<MapHandler<PFP2>*>(mhg_map);
-//        PFP2::MAP* map = mh_map->getMap();
+    if(m_decomposition)
+    {
+        MapHandlerGen* mhg_map = m_schnapps->addMap(mapName, 2);
+        MapHandler<PFP2>* mh_map = static_cast<MapHandler<PFP2>*>(mhg_map);
+        PFP2::MAP* map = mh_map->getMap();
 
-//        VertexAttribute<PFP2::VEC3, PFP2::MAP> position = mh_map->getAttribute<PFP2::VEC3, VERTEX>("position");
-//        if(!position.isValid())
-//        {
-//            position = mh_map->addAttribute<PFP2::VEC3, VERTEX>("position");
-//        }
+        VertexAttribute<PFP2::VEC3, PFP2::MAP> position = mh_map->getAttribute<PFP2::VEC3, VERTEX>("position");
+        if(!position.isValid())
+        {
+            position = mh_map->addAttribute<PFP2::VEC3, VERTEX>("position");
+        }
 
-//        VertexAttribute<ImageCoordinates, PFP2::MAP> imageCoordinates = mh_map->getAttribute<ImageCoordinates, VERTEX>("ImageCoordinates");
-//        if(!imageCoordinates.isValid())
-//        {
-//            imageCoordinates = mh_map->addAttribute<ImageCoordinates, VERTEX>("ImageCoordinates");
-//        }
+        VertexAttribute<PFP2::VEC3, PFP2::MAP> planeCoordinates = mh_map->getAttribute<PFP2::VEC3, VERTEX>("PlaneCoordinates");
+        if(!planeCoordinates.isValid())
+        {
+            planeCoordinates = mh_map->addAttribute<PFP2::VEC3, VERTEX>("PlaneCoordinates");
+        }
 
-//        Decomposition* decomposition = m_decomposition;
-////        while(decomposition->getChild())
-////        {
-////            decomposition = decomposition->getChild();
-////        }
+        VertexAttribute<ImageCoordinates, PFP2::MAP> imageCoordinates = mh_map->getAttribute<ImageCoordinates, VERTEX>("ImageCoordinates");
+        if(!imageCoordinates.isValid())
+        {
+            imageCoordinates = mh_map->addAttribute<ImageCoordinates, VERTEX>("ImageCoordinates");
+        }
 
-//        QImage image = decomposition->getImage();
-//        int imageX = image.width(), imageY = image.height();
+        int image_width = m_decomposition->getWidth(), image_height = m_decomposition->getHeight();
+        int image_min_width = image_width/pow(2, m_decomposition->getLevel()), image_min_height = image_height/pow(2, m_decomposition->getLevel());
 
-//        Algo::Surface::Tilings::Square::Grid<PFP2> grid(*map, imageX-1, imageY-1);
-//        grid.embedIntoGrid(position, imageX-1, imageY-1);
+        Algo::Surface::Tilings::Square::Grid<PFP2> grid(*map, image_min_width-1, image_min_height-1);
+        grid.embedIntoGrid(planeCoordinates, image_width-1, image_height-1);
 
-//        std::vector<Dart> vDarts = grid.getVertexDarts();
+        std::vector<Dart> vDarts = grid.getVertexDarts();
 
-//        mh_map->updateBB(position);
+        for(int i = 0; i < image_min_width; ++i)
+        {
+            for(int j = 0; j < image_min_height; ++j)
+            {
+                imageCoordinates[vDarts[j*image_min_width+i]].setCoordinates(i,image_min_height-j-1);
+            }
+        }
 
-//        qglviewer::Vec bb_max = mh_map->getBBmax();
-//        qglviewer::Vec bb_min = mh_map->getBBmin();
+        mh_map->notifyConnectivityModification();
+        mh_map->notifyAttributeModification(planeCoordinates);
+        mh_map->notifyAttributeModification(imageCoordinates);
 
-//        for(int i = 0; i < imageX; ++i)
-//        {
-//            for(int j = 0; j < imageY; ++j)
-//            {
-//                imageCoordinates[vDarts[j*imageX+i]].setCoordinates(i,imageY-j-1);
-//            }
-//        }
+        m_schnapps->getSelectedView()->updateGL();
 
-//        QImage image_parent = m_decomposition->getImage();
-//        int image_parentX = image_parent.width(), image_parentY = image_parent.height();
-
-//        float width_step_parent = (bb_max.x-bb_min.x)/(image_parentX-1);
-//        float height_step_parent = (bb_max.y-bb_min.y)/(image_parentY-1);
-
-//        int shift_counterX = 0, shift_counterY = 0;
-
-//        while(image_parentX != imageX && image_parentY != imageY)
-//        {
-//            if(image_parentX%2==0)
-//            {
-//                ++shift_counterX;
-//            }
-//            if(image_parentY%2==0)
-//            {
-//                ++shift_counterY;
-//            }
-
-//            image_parentX = image_parentX/2+image_parentX%2;
-//            image_parentY = image_parentY/2+image_parentY%2;
-//        }
-
-//        shift_counterX = pow(2, shift_counterX);
-//        shift_counterY = pow(2, shift_counterY);
-
-//        PFP2::MATRIX44 transform_matrix;
-//        transform_matrix.identity();
-//        transform_matrix.setSubVectorV(0, 3, PFP2::VEC4(-width_step_parent*shift_counterX/2., height_step_parent*shift_counterY/2., 0., 1.));
-//        grid.transform(position, transform_matrix);
-
-//        mh_map->notifyAttributeModification(position);
-//        mh_map->notifyAttributeModification(imageCoordinates);
-//        mh_map->notifyConnectivityModification();
-//        mh_map->updateBB(position);
-
-//        m_schnapps->getSelectedView()->updateGL();
-
-//        m_decomposition = decomposition;
-
-//        return mhg_map;
-//    }
+        return mhg_map;
+    }
     return NULL;
 }
 
 void Surface_WaveletDecomposition_Plugin::project2DImageTo3DSpace(const QString& mapName)
 {
-//    if(m_decomposition)
-//    {
-//        MapHandler<PFP2>* mh_map = static_cast<MapHandler<PFP2>*>(m_schnapps->getMap(mapName));
+    if(m_decomposition)
+    {
+        MapHandlerGen* mhg_map = m_schnapps->getMap(mapName);
+        MapHandler<PFP2>* mh_map = static_cast<MapHandler<PFP2>*>(mhg_map);
 
-//        if(mh_map)
-//        {
-//            PFP2::MAP* map = mh_map->getMap();
+        if(mh_map)
+        {
+            PFP2::MAP* map = mh_map->getMap();
 
-//            if(!m_camera)
-//            {
-//                m_camera = new qglviewer::Camera(*(m_schnapps->getSelectedView()->camera()));
-//                m_camera->setZNearCoefficient(1.f);
-//            }
+            VertexAttribute<PFP2::VEC3, PFP2::MAP> position = mh_map->getAttribute<PFP2::VEC3, VERTEX>("position");
+            if(!position.isValid())
+            {
+                position = mh_map->addAttribute<PFP2::VEC3, VERTEX>("position");
+            }
 
-//            VertexAttribute<PFP2::VEC3, PFP2::MAP> position = mh_map->getAttribute<PFP2::VEC3, VERTEX>("position");
-//            if(!position.isValid())
-//            {
-//                CGoGNerr << "Position attribute is not valid" << CGoGNendl;
-//            }
+            VertexAttribute<PFP2::VEC3, PFP2::MAP> planeCoordinates = mh_map->getAttribute<PFP2::VEC3, VERTEX>("PlaneCoordinates");
+            if(!planeCoordinates.isValid())
+            {
+                planeCoordinates = mh_map->addAttribute<PFP2::VEC3, VERTEX>("PlaneCoordinates");
+            }
 
-//            VertexAttribute<ImageCoordinates, PFP2::MAP> imageCoordinates = mh_map->getAttribute<ImageCoordinates, VERTEX>("ImageCoordinates");
-//            if(!imageCoordinates.isValid())
-//            {
-//                CGoGNerr << "ImageCoordinates attribute is not valid" << CGoGNendl;
-//            }
+            VertexAttribute<ImageCoordinates, PFP2::MAP> imageCoordinates = mh_map->getAttribute<ImageCoordinates, VERTEX>("ImageCoordinates");
+            if(!imageCoordinates.isValid())
+            {
+                imageCoordinates = mh_map->addAttribute<ImageCoordinates, VERTEX>("ImageCoordinates");
+            }
 
-//            QImage image = m_decomposition->getImage();
+            if(!m_camera)
+            {
+                m_camera = new qglviewer::Camera(*(m_schnapps->getSelectedView()->camera()));
+                m_camera->setZNearCoefficient(0.5f);
+            }
 
-//            qglviewer::Vec camera_p = m_camera->position();
+            qglviewer::Vec camera_p = m_camera->position();
 
-//            PFP2::VEC3 camera_position = PFP2::VEC3(camera_p.x, camera_p.y, camera_p.z);
+            PFP2::VEC3 camera_position = PFP2::VEC3(camera_p.x, camera_p.y, camera_p.z);
 
-//            PFP2::VEC3 plane_center_position = camera_position;
-//            plane_center_position[2] -= m_camera->zNear();
+            PFP2::VEC3 plane_center_position = camera_position;
+            plane_center_position[2] -= m_camera->zNear();
 
-//            float d_C_Xc = (plane_center_position-camera_position).norm2();
+            float d_C_Xc = (plane_center_position-camera_position).norm2();
 
-//            //Reprojection des points => Théorème de Thalès
-//            TraversorV<PFP2::MAP> trav_vert_map(*map);
-//            for(Dart d = trav_vert_map.begin(); d != trav_vert_map.end(); d = trav_vert_map.next())
-//            {
-//                PFP2::VEC3 plane_point_position = plane_center_position;
-//                plane_point_position[0] += position[d][0];
-//                plane_point_position[1] += position[d][1];
+            //Reprojection des points => Théorème de Thalès
+            TraversorV<PFP2::MAP> trav_vert_map(*map);
+            for(Dart d = trav_vert_map.begin(); d != trav_vert_map.end(); d = trav_vert_map.next())
+            {
+                PFP2::VEC3 plane_point_position = plane_center_position;
+                plane_point_position[0] += planeCoordinates[d][0];
+                plane_point_position[1] += planeCoordinates[d][1];
 
-//                QRgb pixel = image.pixel(imageCoordinates[d].getXCoordinate(), imageCoordinates[d].getYCoordinate());
+                NQRgb color = m_decomposition->getValue(imageCoordinates[d].getXCoordinate(), imageCoordinates[d].getYCoordinate());
 
-//                float z_coordinate = (m_camera->zFar()-m_camera->zNear())*(1.f-(qRed(pixel)/255.f));
+                float z_coordinate = (m_camera->zFar()-m_camera->zNear())*(1.f-(qAbs(color.getRed())/255.f));
 
-//                PFP2::VEC3 projected_point_position = plane_center_position;
-//                projected_point_position[2] -= z_coordinate;
+                PFP2::VEC3 projected_point_position = plane_center_position;
+                projected_point_position[2] -= z_coordinate;
 
-//                float d_C_Xp = (plane_point_position-camera_position).norm();
-//                float d_C_Xh = (projected_point_position-camera_position).norm2();
+                float d_C_Xp = (plane_point_position-camera_position).norm();
+                float d_C_Xh = (projected_point_position-camera_position).norm2();
 
-//                float d_C_Xe = sqrt(d_C_Xh/d_C_Xc)*d_C_Xp;
+                float d_C_Xe = sqrt(d_C_Xh/d_C_Xc)*d_C_Xp;
 
-//                position[d] = (plane_point_position-camera_position)/d_C_Xp*(d_C_Xe+d_C_Xp);
-//            }
+                position[d] = (plane_point_position-camera_position)/d_C_Xp*(d_C_Xe+d_C_Xp);
+            }
 
-//            mh_map->notifyAttributeModification(position);
-//            mh_map->notifyConnectivityModification();
-//            mh_map->updateBB(position);
-
-//            //camera->setSceneBoundingBox(mh_map->getBBmin(), mh_map->getBBmax());
-
-//            m_schnapps->getSelectedView()->camera()->setSceneBoundingBox(mh_map->getBBmin(), mh_map->getBBmax());
-
-//            m_schnapps->getSelectedView()->updateGL();
-//        }
-//    }
+            mh_map->notifyAttributeModification(position);
+            mh_map->updateBB(position);
+            m_schnapps->getSelectedView()->updateGL();
+        }
+    }
 }
 
 void Surface_WaveletDecomposition_Plugin::triangulateMap(const QString& mapName)
 {
-//    if(m_decomposition)
-//    {
-//        MapHandler<PFP2>* mh_map = static_cast<MapHandler<PFP2>*>(m_schnapps->getMap(mapName));
+    if(m_decomposition)
+    {
+        MapHandler<PFP2>* mh_map = static_cast<MapHandler<PFP2>*>(m_schnapps->getMap(mapName));
 
-//        if(mh_map)
-//        {
-//            PFP2::MAP* map = mh_map->getMap();
-//            VertexAttribute<PFP2::VEC3, PFP2::MAP> position = mh_map->getAttribute<PFP2::VEC3, VERTEX>("position");
-//            if(!position.isValid())
-//            {
-//                CGoGNerr << "Position attribute is not valid" << CGoGNendl;
-//            }
+        if(mh_map)
+        {
+            PFP2::MAP* map = mh_map->getMap();
+            VertexAttribute<PFP2::VEC3, PFP2::MAP> position = mh_map->getAttribute<PFP2::VEC3, VERTEX>("position");
+            if(!position.isValid())
+            {
+                CGoGNerr << "Position attribute is not valid" << CGoGNendl;
+            }
 
-//            DartMarker<PFP2::MAP> marker(*map);
+            DartMarker<PFP2::MAP> marker(*map);
 
-//            TraversorF<PFP2::MAP> trav_face_map(*map);
-//            for(Dart d = trav_face_map.begin(); d != trav_face_map.end(); d = trav_face_map.next())
-//            {
-//                if(!marker.isMarked(d))
-//                {
-//                    Dart phi_11_d = map->phi<11>(d);
-//                    map->splitFace(d, phi_11_d);
-//                    marker.markOrbit<FACE>(d);
-//                    marker.markOrbit<FACE>(phi_11_d);
-//                }
-//            }
+            TraversorF<PFP2::MAP> trav_face_map(*map);
+            for(Dart d = trav_face_map.begin(); d != trav_face_map.end(); d = trav_face_map.next())
+            {
+                if(!marker.isMarked(d))
+                {
+                    Dart phi_11_d = map->phi<11>(d);
+                    map->splitFace(d, phi_11_d);
+                    marker.markOrbit<FACE>(d);
+                    marker.markOrbit<FACE>(phi_11_d);
+                }
+            }
 
-//            mh_map->notifyAttributeModification(position);
-//            mh_map->notifyConnectivityModification();
+            mh_map->notifyAttributeModification(position);
+            mh_map->notifyConnectivityModification();
 
-//            m_schnapps->getSelectedView()->updateGL();
-//        }
-//    }
+            m_schnapps->getSelectedView()->updateGL();
+        }
+    }
 }
 
 void Surface_WaveletDecomposition_Plugin::moveUpDecomposition(const QString& mapName)
